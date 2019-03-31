5bda2daccc71a944cb9aa534488f683b75fbcae2
Aaron <Apowell829@gmail.com>
2019-03-31 19:07:29 +0100

    Added close animation for more button

--- resources/js/main.js
+++ resources/js/main.js
@@ -6,33 +6,34 @@ document.querySelector(".title").addEventListener("click", function() {
   this.classList.toggle("text-flicker-in-glow");
 });
 
+/*
+let icons = document.querySelector(".icons");
+icons.style.opacity = "1";
+icons.classList.add("scale-in-center");
+*/
 (async function animateIcons() {
   for (let index = 0; index < 4; index++) {
     let icon = document.querySelector(`.animatedIcon-${index}`);
     icon.style.opacity = "1";
     let elements = icon.children;
     elements[0].classList.add("scale-in-center");
-    await sleep(500);
+    await sleep(200);
   }
 })();
 
-const icons = document.querySelector(".icons").children;
-console.log(icons[1]);
-for (i = 0; i < icons.length; i++) {
-  icons[i].addEventListener("mouseover", async function() {
-    this.classList.add("text-pop-up-top");
-    await sleep(1000);
-    this.classList.remove("text-pop-up-top");
-  });
-}
-/* icons[i].addEventListener("mouseleave", function() {
-    this.classList.toggle("text-pop-up-top");
-  });
-} //llist of icons*/
+const moredots = document.querySelector(".more-dots");
+moredots.addEventListener(
+  "click",
+   function() {
+    this.firstElementChild.classList.toggle("open");
+    this.firstElementChild.firstElementChild.classList.toggle("fa-bars");
+  },
+  true
+);
 
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {
-  myFunction();
+  stickyFunc();
   sectionDetection();
 };
 
@@ -42,7 +43,7 @@ var navbar = document.querySelector(".sidebar");
 var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+function stickyFunc() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky");
   } else {
@@ -56,12 +57,20 @@ function removeActiveClass() {
     el.classList.remove("side-nav__item--active");
   });
 }
+/*
 sideNavItems.forEach(el =>
   el.addEventListener("click", () => {
     removeActiveClass();
     el.classList.add("side-nav__item--active");
   })
 );
+*/
+for (let i = 0; i < sideNavItems.length - 1; i++) {
+  sideNavItems[i].addEventListener("click", () => {
+    removeActiveClass();
+    sideNavItems[i].classList.add("side-nav__item--active");
+  });
+}
 
 var sections = document.querySelectorAll("section");
 let section1 = sections[0].offsetTop - 60;
